26e5a6367893cc67fc71fc898c750752ebf9b8a2
gubong <105339056+gubong@users.noreply.github.com>
2025-07-22 15:54:02 +0900

    Add tarot card data (메이저 아르카나 예시)

---
+++ src/data/tarotCards.js
@@ -0,0 +1,24 @@
+export const tarotCards = [
+  {
+    id: 0,
+    name: "The Fool",
+    image: "/cards/0-the-fool.png",
+    upright: "새로운 시작, 순수, 자유, 잠재력",
+    reversed: "무모함, 방향 상실, 경솔함"
+  },
+  {
+    id: 1,
+    name: "The Magician",
+    image: "/cards/1-the-magician.png",
+    upright: "의지, 창조력, 자원, 집중력",
+    reversed: "속임수, 기회 상실, 의지 부족"
+  },
+  {
+    id: 2,
+    name: "The High Priestess",
+    image: "/cards/2-the-high-priestess.png",
+    upright: "직관, 잠재의식, 신비, 내면의 소리",
+    reversed: "비밀, 혼란, 자기기만, 내면과의 단절"
+  }
+  // ... (총 22장 메이저 아르카나 데이터 예시)
+];
